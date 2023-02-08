16a57256487feb6fabb1ba09e27e5a1563a29881
lurbano <lurbano@earthsciweb.org>
2023-02-08 14:34:48 -0600

    test

--- test2.php
+++ test2.php
@@ -1,9 +1,13 @@
 <?php
 
     echo "Hi <em>Brooke</em>, Why are you late??? "; 
-    echo "And you too Coen!!!";
-    print_r($_GET);
+    echo "And you too Coen!!!<br>";
+    print_r($_GET) . "<br>";
     echo "Hello " . $_GET["name"];
+
+    foreach ($_GET as $id){
+        echo $id . "==>" . $_GET[$id] . "<br>";
+    }
 
     $f = fopen("test.json", "w");
     fwrite($f, "This is a file.");
