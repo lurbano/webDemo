23559c87b4f6d1831db35e43f8902952d6b74bb7
lurbano <lurbano@earthsciweb.org>
2023-02-08 14:26:19 -0600

    f

---
+++ test2.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "Hi <em>Brooke</em>, Why are you late??? "; 
+    echo "And you too Coen!!!";
+    print_r($_GET);
+
+    $f = fopen("test.json", "w");
+    fwrite($f, "This is a file.");
+    fclose($f);
+    echo "Saved";
+
+?>
